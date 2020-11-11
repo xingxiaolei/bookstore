0171778fd20b208296d62c40f372dd7fc4109a43
xxl <465123144@qq.com>
2020-11-11 18:50:50 +0800

    该 去付款 了

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^login_check/$', views.login_check, name='login_check'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^$', views.user, name='user'),
+    url(r'^address/$', views.address, name='address'),
+    url(r'^order/(?P<page>\d+)?/?$', views.order, name='order'),
 ]
